7414079801cb35a18ce791c2a9e4e0cecf4d99cc
李民凯 <473759681@qq.com>
2018-09-25 11:01:16 +0800

    Initial Commit

---
+++ Ukokehome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ukokehome
+//
+//  Created by ethome on 2018/9/25.
+//  Copyright © 2018年 ethome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ukokehome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ukokehome
+//
+//  Created by ethome on 2018/9/25.
+//  Copyright © 2018年 ethome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
